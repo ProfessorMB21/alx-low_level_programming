c4caf2ad67a5a8a95f9d8d2e0d041fdcf0befe0b
Bupe M <profbupeeli@gmail.com>
2023-06-15 12:54:20 -0700

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program to print to the console using printf
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s, %s,\n", "with proper grammar", "but the outcome is a piece of art");
+	return (0);
+}
