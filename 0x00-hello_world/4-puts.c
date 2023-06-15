082186f5dc9a284326af3b36eea95432f3fd8068
Bupe M <profbupeeli@gmail.com>
2023-06-15 12:38:47 -0700

    Fix bug in documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/*
- * main - Entry point
+/**
+ * main - A program that uses puts to print a line.
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle.\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle.\n");
+	return (0);
 }
