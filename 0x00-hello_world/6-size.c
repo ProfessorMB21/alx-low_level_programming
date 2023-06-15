e17b14517f9cbce8f8f7da1931d5f4ea4bb01af0
Bupe M <profbupeeli@gmail.com>
2023-06-15 15:29:29 -0700

    Fix bug by removing \n in last print statement

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
  * main - This program prints the sizes of various types on the machine it
- * 	  is compiled and run on.
+ *is compiled and run on.
  * 
  * Return: Always 0 (Success)
  */
@@ -19,6 +19,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of an float: %d byte(s)\n", sizeof(float));
+	printf("Size of an float: %d byte(s)", sizeof(float));
 	return (0);
 }
