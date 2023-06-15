ecf0876415e67136af5da3184573dc06603eade7
Bupe M <profbupeeli@gmail.com>
2023-06-15 15:15:19 -0700

    Fix bug in 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,15 +4,21 @@
  * main - This program prints the sizes of various types on the machine it
  * 	  is compiled and run on.
  * 
- * Return: Always 0. (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+	int i;
+	float f;
+	long int li;
+	long long int lli;
+	char c;
+
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of an float: %d byte(s)\n", sizeof(float);
+	printf("Size of an float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
