4f2f44ce7fb1046ad78ef49a01da9599ba4d0441
Bupe M <profbupeeli@gmail.com>
2023-06-20 16:57:17 -0700

    Add: 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+/* Preprocessors */
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase using user defined header
+ *
+ * Return: 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	/* declarations */
+	char c;
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define main_H
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
