e9125dfbeca2552dc37a38c561104f57ae00ac8f
Bupe M <profbupeeli@gmail.com>
2023-06-20 14:15:34 -0700

    Add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+/* Preprocessors */
+#include <stdio.h>
+
+/**
+ * main - prints the string '_putchar' to the terminal
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", "_putchar");
+	return (0);
+}
