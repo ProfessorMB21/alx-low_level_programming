6a660612e906c9c1be1a0191c9a32dbe57b3a647
Bupe M <profbupeeli@gmail.com>
2023-06-21 13:57:22 -0700

    Add: 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+/* Preprocessors */
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	/* declarations */
+	int n, m, prod;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+			prod = m * n;
+			_putchar(prod + '0');
+			if (n == 9)
+			{
+				continue;	/* prevent trailing spaces */
+			} else
+			{
+				_putchar(',');
+				_putchar('\t');
+			}
+			_putchar('\n');
+		}
+	}
+}
