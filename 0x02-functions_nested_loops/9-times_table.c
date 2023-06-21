5493b0df177c819367fdd3e5f1654cb051c34c92
Bupe M <profbupeeli@gmail.com>
2023-06-21 14:09:53 -0700

    Fix: bug on stdout

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,6 +18,7 @@ void times_table(void)
 		{
 			prod = m * n;
 			_putchar(prod + '0');
+			_putchar(prod / 10 + '0');
 			if (n == 9)
 			{
 				continue;	/* prevent trailing spaces */
@@ -26,7 +27,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar('\t');
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
