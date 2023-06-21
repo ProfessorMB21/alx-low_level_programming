c61971e1d199b8f840a27080f037b977454b45c5
Bupe M <profbupeeli@gmail.com>
2023-06-20 18:20:37 -0700

    Add: 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+/* Preprocessors */
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: int
+ *
+ * Return: 1 (if positive), 0 (if zero), -1 (if negative)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	} else if (n < 0)
+	{
+		return (-1);
+	}
+	return (0)
+}
