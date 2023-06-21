27973708665c6888c2ba6c3b043eb00e61cac747
Bupe M <profbupeeli@gmail.com>
2023-06-20 17:21:26 -0700

    Fix: Removed trailing spaces and fixed newline bug

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 	/* declarations */
 	int c;
 	int i;
-	
+
 	i = 0;
 	while (i <= 10)
 	{
@@ -20,7 +20,7 @@ void print_alphabet_x10(void)
 		{
 			_putchar(c);
 		}
+		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
 }
