bb0389360e2e4917b7b8dc507d5a65ab744092aa
Bupe M <profbupeeli@gmail.com>
2023-06-21 12:03:09 -0700

    Add: 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+/* Preprocessors */
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	/* declarations */
+	int i, j, k, m, hours;
+
+	hours = 10;
+	for (i = 0; i < 3; i++)
+	{
+		if (i == 2)
+		{
+			hours = 4;
+		}
+		for (j = 0; j < hours; j++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (m = 0; m < 10; m++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(m);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
