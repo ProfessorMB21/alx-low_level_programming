7028811098592451b892710ac83aeeef2bf292cd
Bupe M <profbupeeli@gmail.com>
2023-06-20 19:01:45 -0700

    Add: 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+/* Preprocessors */
+#include "main.h"
+
+/**
+ * prnt_last_digit - Prints the last digit of a number
+ * @i: int
+ *
+ * Return: r (success)
+ */
+
+int print_last_digit(int i)
+{
+	/* declarations */
+	int r = i % 10;
+
+	return (r);
+}
