e7a7d8e05127feaebaafee70a88e345a7ebc5670
Bupe M <profbupeeli@gmail.com>
2023-06-21 15:55:53 -0700

    Add: 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/* Preprocessor */
+#include "main.h"
+
+/**
+ * add - Computes the sum of two integers
+ * @n: int
+ * @m: int
+ *
+ * Return: sum (success)
+ */
+
+int add(int n, int m)
+{
+	/* declarations */
+	int sum = m + n;
+
+	return (sum);
+}
