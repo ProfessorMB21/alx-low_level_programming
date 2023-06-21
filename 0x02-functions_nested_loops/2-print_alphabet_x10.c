c10edcd52758eff83dc8148d850ea8b5010f4150
Bupe M <profbupeeli@gmail.com>
2023-06-20 17:12:02 -0700

    Add: 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+/* Preprocessors */
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	/* declarations */
+	char c;
+	int i;
+
+	while (i <= 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
