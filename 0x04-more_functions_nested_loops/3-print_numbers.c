cdc26f6e1439d3b1a0b6d08de1f077bdbc2c7287
Bupe M <profbupeeli@gmail.com>
2023-06-22 16:48:17 -0700

    Fix: implemented correct syntax

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -12,9 +12,9 @@ void print_numbers(void)
 	/* declarations */
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(i + '0');
+		_putchar(i);
 	}
 	_putchar('\n');
 }
