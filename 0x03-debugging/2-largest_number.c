fb646a950d60f75ce87e3514cd8de814d1dfb423
Bupe M <profbupeeli@gmail.com>
2023-06-23 13:56:58 -0700

    Fix: removed conditional

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,13 +20,9 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if ( c > a && a > b)
-	{
-		largest = c;
-	}
 	else
 	{
-		largest = 0;
+		largest = c;
 	}
 
 	return (largest);
