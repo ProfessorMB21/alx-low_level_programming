29ca2c4f3bc7ef072ad54e08b08dbc884fd2435f
Bupe M <profbupeeli@gmail.com>
2023-06-22 19:02:05 -0700

    Add: 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+/* Preprocessors */
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz test
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	/* declarations */
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		} else if (i % 3 == 0)
+		{
+			printf("%S", "Fizz"):
+		} else if (i % 5 == 0)
+		{
+			printf("%s", "Buzz");
+		} else
+		{
+			printf("%d", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
