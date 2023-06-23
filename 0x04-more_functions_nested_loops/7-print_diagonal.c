a25c8cf82c314c1f560efbcc5468e549e6fc5a15
Bupe M <profbupeeli@gmail.com>
2023-06-22 18:43:20 -0700

    Fix: added for loop to add whitespace

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,12 +11,16 @@
 void print_diagonal(int n)
 {
 	/* declarations */
-	int i;
+	int i, j;
 
 	if (n != 0)
 	{
 		for (i = 0; i < n; i++)
 		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\\');
 			_putchar('\n');
 		}
