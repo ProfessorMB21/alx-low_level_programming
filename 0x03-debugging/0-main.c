3780ed08b183a83fb40af2ccfd12080c967d1e05
Bupe M <profbupeeli@gmail.com>
2023-06-23 09:19:03 -0700

    Fix: made some changes

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
-/* Preprocessors */
 #include "main.h"
 
 /**
  * main - tests function that prints if integer is negative or positive
- *
  * Return: 0 (success)
  */
 
 int main(void)
 {
-	/* declarations */
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
