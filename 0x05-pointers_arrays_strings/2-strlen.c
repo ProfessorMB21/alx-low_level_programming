c7f67d860b14a8d150938589197eaeac218051d3
Bupe M <profbupeeli@gmail.com>
2023-06-27 09:20:30 -0700

    Add: 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen - Returns the length of a string
+ * @s: ptr to int
+ * Return: len
+ */
+
+int _strlen(int *s)
+{
+	/* declarations */
+	int len = 0;
+
+	while (i != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
