95fe30471456d7108ef4fb00cb1c32b916cc6102
Bupe M <profbupeeli@gmail.com>
2023-06-28 02:39:06 +0300

    Fix: added _strlen function to support

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,14 +12,22 @@ void print_rev(char *s)
 	/* declarations */
 	int i, len;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-			len++;
-	}
-
+	len = _strlen(s);
 	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
 	_putchar('\n');
+}
+
+int _strlen(char *s)
+{
+	/* declarations */
+	int len;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+			len++;
+	}
+	return (len);
 }
