3eafa18562163a82a8611bed310407cd12085bd8
Bupe M <profbupeeli@gmail.com>
2023-06-27 17:50:07 -0700

    Fix: fixed initialization

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,8 +15,9 @@ void rev_string(char *s)
 	char c;
 
 	len = strlen(s);
-	for (i = 0, j = len - 1; i < j; i++, j--)
+	for (i = 0, j = len - 1; i < j; ++i, --j)
 	{
+		c = s[i];
 		s[i] = s[j];
 		s[j] = c;
 	}
