f8a21705225225b44e61ef8d0f4ecdd73aa975d2
Bupe M <profbupeeli@gmail.com>
2023-06-27 17:35:32 -0700

    Add: 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+/* Preprocessors */
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - Reverses a string
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	/* declarations */
+	int len, i, j;
+	char c;
+
+	len = strlen(s);
+	for (i = 0; j = len - 1; i < j; i++, j--)
+	{
+		s[i] = s[j];
+		s[j] = c;
+	}
+}
