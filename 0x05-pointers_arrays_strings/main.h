6113c49c8c2b1417b51b4057e8e6d5a17704e1c2
Bupe M <profbupeeli@gmail.com>
2023-06-27 18:35:58 -0700

    Update: added new function declarations

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,6 +9,9 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 void _putchar(char c);
 
 #endif
