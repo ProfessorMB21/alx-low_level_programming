82a6c6178fcfb40657d4fecfbdeb00093ba0d9b6
Bupe M <profbupeeli@gmail.com>
2023-06-30 09:12:40 -0700

    Fix: added another iterator to check for characters in a find and str arrays

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,26 +4,33 @@
 /**
  * leet - Encodes a string by replacing letters with numbers
  *@str: ptr, string
- * Return: 0 (success)
+ * Return: str
  */
 
 char *leet(char *str)
 {
 	/* declarations */
-	int i;
+	int i, j;
+	char find[] = "aAeEoOtTlL";
+	char replace[] = "4433007711";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		else if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		else if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		else if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		else if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+		/**
+		 * look through the find array
+		 * and check if a character is
+		 * equal to the character in
+		 * str array at i and replace
+		 * it with its corresponding
+		 * value in the find array
+		 */
+		for (j = 0; find[j] != '\0'; j++)
+		{
+			if (str[i] == find[j])
+			{
+				str[i] = replace[j];
+			}
+		}
 	}
-	return (0);
+	return (str);
 }
