a7271910bc1c0b0c3b30c7aff76094a032605de6
Bupe M <profbupeeli@gmail.com>
2023-07-03 13:19:45 -0700

    Update: corrected function declaration

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@
 int _putchar(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *strchr(char *s, char c);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
