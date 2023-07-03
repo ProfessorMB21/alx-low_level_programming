908cefa450ea6a5d53c9aef30a57170153d3ff56
Bupe M <profbupeeli@gmail.com>
2023-07-03 13:41:54 -0700

    Fix: used while

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,19 @@
 
 char *_strchr(char *s, char c)
 {
-	/* declarations */
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-			return (s[i]);
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
 	}
+
+	if (*s == c)
+	{
+		return (s);
+	}
+
 	return (0);
 }
