aab896b0c7c283e0804b8adc6594a9ebb15f928b
Bupe M <profbupeeli@gmail.com>
2023-07-04 16:42:15 -0700

    Update: corrected arg in function declaration

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@
  * Prototype functions
  */
 
-int _putchar(char *s);
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
