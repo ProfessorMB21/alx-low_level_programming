77cd3ff93d8278ad6cc6aaf393653c76b0dcb999
Bupe M <profbupeeli@gmail.com>
2023-07-08 15:53:04 -0700

    Changed logic

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,31 +3,6 @@
 #include <stdlib.h>
 
 /**
- * isNum - Checks if is a digit
- * @s: ptr, character
- * Return: 0 (success), otherwise 1
- */
-
-int isNum(char *s)
-{
-	/* declarations */
-	int i;
-
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		if (*(s + i) >= '0' && *(s + i) <= '9')
-		{
-			i++;
-		} else
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
-
-
-/**
  * main - Adds positive numbers
  * @argc: Number of arguments passed
  * @argv: ptr, Array of arguments
@@ -37,7 +12,7 @@ int isNum(char *s)
 int main(int argc, char *argv[])
 {
 	/* declarations */
-	int i, count = 0;
+	int i, j, count = 0;
 
 	/**
 	 * if no args passed prints
@@ -45,25 +20,22 @@ int main(int argc, char *argv[])
 	 */
 	if (argc == 1)
 	{
-		count = 0;
 		printf("%d\n", count);
 	} else if (argc > 1)
 	{
-		i = 1;
-		while (i < argc)
+		for (i = 1; i < argc; i++)
 		{
-			if (isNum(argv[i]) == 0)
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				count += atoi(argv[i]);
-			} else
-			{
-				printf("Error\n");
-				return (1);
+				if (argv[i][j] > '9' || argv[i][j] < '0')
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			i++;
+			count += atoi(argv[i]);
 		}
 		printf("%d\n", count);
 	}
-
 	return (0);
 }
