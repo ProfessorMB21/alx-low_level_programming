abcc5160d7eea57e2023d9ae5b3c2ccf91b8c1ac
Bupe M <profbupeeli@gmail.com>
2023-11-03 02:23:44 +0300

    update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,11 @@
-#include "main.h"
+#include <stdlib.h>
 
 /**
 * malloc_checked - Returns a pointer to allocated memory
 * @b: non-zero number of bytes to be allocated
 * Return: pointer to allocated memory, otherwise return 98
 */
+
 void *malloc_checked(unsigned int b)
 {
 	/* declarations */
