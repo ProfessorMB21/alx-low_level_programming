d65586b5f9bc49203e6e0ef32ae3295a759dfd95
Bupe M <profbupeeli@gmail.com>
2023-11-11 20:30:33 +0300

    update file (fix bug)

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,10 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 	
-	if (array == NULL || action == NULL || size < 0)
+	if (array == NULL || action == NULL)
 		return;
 	
-	for (i = 0; i < size, i++)
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
