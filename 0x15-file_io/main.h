e7411cfcaae77de84133d80f83365ec530df5a74
Bupe M <profbupeeli@gmail.com>
2023-12-01 20:13:09 +0300

    add file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <elf.h>
+
+/* prototypes */
+ssize_t read_textfile(const char* filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /** FILE_IO_MAIN_H */
