049d868df40933c272af56177b3263cb1eea0836
Bupe M <profbupeeli@gmail.com>
2024-03-15 02:25:35 +0300

    fixed more bugs

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	
 	ht->size = size;
-	ht->array = malloc(sizeof(hast_node_t *) * size);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
