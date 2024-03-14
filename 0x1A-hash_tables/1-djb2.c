adca778c2546de2dabaa80750e36bf4565bfa47b
Bupe M <profbupeeli@gmail.com>
2024-03-15 02:37:13 +0300

    added parentheses

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -12,7 +12,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	unsigned long int hash = 5381;
 	int c;
 
-	while (c = *str++)
+	while ((c = *str++))
 		hash = ((hash << 5) + hash) + c;
 
 	return (hash);
